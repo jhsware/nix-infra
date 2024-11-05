73c8740bf3246840c5c7fc0a1b289cf171745789
Sebastian Ware <sebastian@urbantalk.se>
2024-11-05 18:34:11 +0100

    Only openssh on non-macos systems

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-
+  isMacOS = builtins.match ".*-darwin" pkgs.stdenv.hostPlatform.system != null;
 in pkgs.mkShell rec {
   name = "dart";
 
   buildInputs = with pkgs; [    
     pkgs.dart
     pkgs.openssl
+  ] ++ (if !isMacOS then [
     pkgs.openssh
-  ];
+  ] else []);
 
   shellHook = ''
 
